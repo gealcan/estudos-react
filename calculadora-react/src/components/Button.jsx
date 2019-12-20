087598604b532492cce16fefcda0db8f08ea4a4d
gealcan <gealcan.neto@gmail.com>
2019-12-20 17:11:37 -0300

    fim calculadora

--- calculadora-react/src/components/Button.jsx
+++ calculadora-react/src/components/Button.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './Button.css'
 
 export default props => {
+let operation = "";
+
+if (props.op) {
+    operation = props.op;
+} else {
+    operation = props.label;
+}
+    
     return (
     <button 
-        onClick={e => props.click && props.click(props.label)}
+        onClick={e => props.click && props.click(operation)}
         className={`
         button
         ${props.operation ? 'operation' : ''}
--- calculadora-react/src/main/Calculator.jsx
+++ calculadora-react/src/main/Calculator.jsx
@@ -5,7 +5,7 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 const initialState = {
-    displayValue: '1000',    // valor que aparece no display
+    displayValue: '0',    // valor que aparece no display
     clearDisplay: false,  // limpar display
     operation: null,    //   armazena operação
     values: [0, 0],   // array com os dois valores
@@ -23,6 +23,31 @@ export default class Calculator extends Component {
 
     setOperation(operation) {
         console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+            
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+
+
+            })
+        }
     }
 
     addDigit(n) {
@@ -33,7 +58,17 @@ export default class Calculator extends Component {
         const clearDisplay = this.state.displayValue === '0'   // limpar display quando tiver 0 ou quando clearDisplay for true
             || this.state.clearDisplay
         
-        const currentValue = clearDisplay ? '' : this.state.displayValue 
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false})
+        
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     }
 
     render() {
@@ -47,7 +82,7 @@ export default class Calculator extends Component {
                 <Button label="7" click={addDigit}/>
                 <Button label="8" click={addDigit}/>
                 <Button label="9" click={addDigit}/>
-                <Button label="*" click={setOperation} operation/>
+                <Button label="x" op={"*"} click={setOperation} operation/>
                 <Button label="4" click={addDigit}/>
                 <Button label="5" click={addDigit}/>
                 <Button label="6" click={addDigit}/>
